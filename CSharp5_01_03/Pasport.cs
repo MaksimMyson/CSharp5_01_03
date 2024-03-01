c6d326ae287c7ef487f356eb500036323b3689b2
MaksimMyson <maksimmyson33@gmail.com>
2024-03-01 20:21:17 +0200

    exe2

---
+++ CSharp5_01_03/Pasport.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp5_01_03
+{
+    public interface IDisplayable
+    {
+        void DisplayInfo();
+    }
+
+    
+    public class Passport : IDisplayable
+    {
+        public string FullName { get; set; }
+        public string Nationality { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        
+        public Passport(string fullName, string nationality, DateTime dateOfBirth)
+        {
+            FullName = fullName;
+            Nationality = nationality;
+            DateOfBirth = dateOfBirth;
+        }
+
+        
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Full Name: {FullName}");
+            Console.WriteLine($"Nationality: {Nationality}");
+            Console.WriteLine($"Date of Birth: {DateOfBirth.ToShortDateString()}");
+        }
+    }
+
+    
+    public class ForeignPassport : Passport, IDisplayable
+    {
+        public string PassportNumber { get; set; }
+        public List<string> Visas { get; set; }
+
+       
+        public ForeignPassport(string fullName, string nationality, DateTime dateOfBirth, string passportNumber)
+            : base(fullName, nationality, dateOfBirth)
+        {
+            PassportNumber = passportNumber;
+            Visas = new List<string>();
+        }
+
+        
+        public void AddVisa(string visa)
+        {
+            Visas.Add(visa);
+        }
+
+        
+        public new void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Passport Number: {PassportNumber}");
+            Console.WriteLine("Visas:");
+            foreach (var visa in Visas)
+            {
+                Console.WriteLine(visa);
+            }
+        }
+    }
+}
--- CSharp5_01_03/Program.cs
+++ CSharp5_01_03/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace CSharp5_01_03
+﻿using CSharp5_01_03;
+using System;
+
+namespace PassportProgram
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Passport passport = new Passport("John Doe", "USA", new DateTime(1990, 5, 15));
+            ForeignPassport foreignPassport = new ForeignPassport("Jane Smith", "Canada", new DateTime(1985, 10, 20), "AB123456");
+
+            foreignPassport.AddVisa("Schengen Visa");
+            foreignPassport.AddVisa("UK Visa");
+
+            Console.WriteLine("Passport Information:");
+            passport.DisplayInfo();
+            Console.WriteLine();
+
+            Console.WriteLine("Foreign Passport Information:");
+            foreignPassport.DisplayInfo();
         }
     }
 }

c644ffcddf05fbb389c32376853804cdaf1f1bfc
MaksimMyson <maksimmyson33@gmail.com>
2024-03-02 15:52:12 +0200

    exe-3

---
+++ CSharp5_01_03/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp5_01_03
+{
+    public interface IAnimal
+    {
+        void DisplayInfo();
+    }
+
+   
+    public class Animal : IAnimal
+    {
+        
+        protected string name;
+        protected string species;
+
+        
+        public Animal(string name, string species)
+        {
+            this.name = name;
+            this.species = species;
+        }
+
+        
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Ім'я: {name}");
+            Console.WriteLine($"Вид: {species}");
+        }
+    }
+}    
+    
--- CSharp5_01_03/Program.cs
+++ CSharp5_01_03/Program.cs
@@ -1,10 +1,45 @@
 ﻿namespace CSharp5_01_03
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            Tiger tiger = new Tiger("Шерхан", "Тигр");
+            Crocodile crocodile = new Crocodile("Алігатор", "Крокодил");
+            Kangaroo kangaroo = new Kangaroo("Скіппі", "Кенгуру");
+
+            
+            tiger.DisplayInfo();
+            Console.WriteLine();
+            crocodile.DisplayInfo();
+            Console.WriteLine();
+            kangaroo.DisplayInfo();
+        }
+    }
+
+    public class Tiger : Animal
+    {
+        public Tiger(string name, string species) : base(name, species)
+        {
+        }
+    }
+
+   
+    public class Crocodile : Animal
+    {
+        public Crocodile(string name, string species) : base(name, species)
+        {
+        }
+    }
+
+  
+    public class Kangaroo : Animal
+    {
+        public Kangaroo(string name, string species) : base(name, species)
+        {
         }
     }
 }

a3a6c722957261c6524f232a05f9a48b62337d46
MaksimMyson <maksimmyson33@gmail.com>
2024-03-02 17:17:12 +0200

    exe2

---
+++ CSharp5_01_03/Devices.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharp5_01_03
+{
+    public class Kettle : Device
+    {
+        public Kettle(string name, string characteristics) : base(name, characteristics)
+        {
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine($"{Name}: Шипіння кип'ятка");
+        }
+    }
+
+    public class Microwave : Device
+    {
+        public Microwave(string name, string characteristics) : base(name, characteristics)
+        {
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine($"{Name}: Гудіння мікрохвильовки");
+        }
+    }
+
+    public class Car : Device
+    {
+        public Car(string name, string characteristics) : base(name, characteristics)
+        {
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine($"{Name}: Рев автомобіля");
+        }
+    }
+
+    public class Steamboat : Device
+    {
+        public Steamboat(string name, string characteristics) : base(name, characteristics)
+        {
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine($"{Name}: Гудіння пароплава");
+        }
+    }
+}
--- CSharp5_01_03/Program.cs
+++ CSharp5_01_03/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            
+            Device[] devices = new Device[4];
+
+            Console.WriteLine("Введіть назву та характеристики кожного пристрою:");
+
+            for (int i = 0; i < devices.Length; i++)
+            {
+                Console.WriteLine($"Пристрій #{i + 1}:");
+                Console.Write("Назва: ");
+                string name = Console.ReadLine();
+                Console.Write("Характеристики: ");
+                string characteristics = Console.ReadLine();
+
+                switch (i)
+                {
+                    case 0:
+                        devices[i] = new Kettle(name, characteristics);
+                        break;
+                    case 1:
+                        devices[i] = new Microwave(name, characteristics);
+                        break;
+                    case 2:
+                        devices[i] = new Car(name, characteristics);
+                        break;
+                    case 3:
+                        devices[i] = new Steamboat(name, characteristics);
+                        break;
+                }
+            }
+
+          
+            Console.WriteLine("\nЗвук кожного пристрою:");
+            foreach (Device device in devices)
+            {
+                device.Sound();
+            }
         }
     }
 }
